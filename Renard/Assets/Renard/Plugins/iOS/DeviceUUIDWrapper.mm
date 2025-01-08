1012ac1bc830d7671b370a373abd15f905d70cc9
mlp-h-morinaga <h.morinaga@meleap.com>
2025-01-08 14:46:39 +0900

    UUID取得のネイティブコードを修正

--- Renard/Assets/Renard/Plugins/iOS/DeviceUUIDWrapper.mm
+++ Renard/Assets/Renard/Plugins/iOS/DeviceUUIDWrapper.mm
@@ -3,15 +3,15 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-	+ (const char *)getPersistentUUID:(const char *)key {
-	   NSString *keyString = [NSString stringWithUTF8String:key];
+    const char* getPersistentUUID(const char* key) {
+       NSString *keyString = [NSString stringWithUTF8String:key];
            NSString *uuid = [DeviceUUID getPersistentUUIDForKey:keyString];
-	   return [uuid UTF8String];
-	}
-	+ void deletePersistentUUID:(const char *)key {
-	   NSString *keyString = [NSString stringWithUTF8String:key];
-	   [DeviceUUID deletePersistentUUID:keyString];
-	}
+       return [uuid UTF8String];
+    }
+    void deletePersistentUUID(const char* key) {
+       NSString *keyString = [NSString stringWithUTF8String:key];
+       [DeviceUUID deletePersistentUUIDForKey:keyString];
+    }
 #ifdef __cplusplus
 }
 #endif
